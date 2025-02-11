3dc898186f55844d3799ef6e97687784058e0482
nandosannn <silvafernandosan@gmail.com>
2025-02-11 10:07:41 -0300

    Finalização primeiro módulo

--- produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -3,14 +3,17 @@ package io.github.cursodsousa.produtosapi.controller;
 import io.github.cursodsousa.produtosapi.model.Produto;
 import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -43,5 +46,18 @@ public class ProdutoController {
     @DeleteMapping("{id}")
     public void deletar(@PathVariable("id") String id) {
         produtoRepository.deleteById(id);
+    }
+    
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id,
+                          @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 }
--- produtosapi/src/main/java/io/github/cursodsousa/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/repository/ProdutoRepository.java
@@ -1,8 +1,11 @@
 package io.github.cursodsousa.produtosapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.cursodsousa.produtosapi.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+    List<Produto> findByNome(String nome);
 }
