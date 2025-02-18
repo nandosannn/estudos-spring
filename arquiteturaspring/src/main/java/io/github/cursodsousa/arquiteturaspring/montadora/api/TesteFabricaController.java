aec1b36fb1f5947321de05f709aeb8e3c39cfc08
nandosannn <silvafernandosan@gmail.com>
2025-02-18 09:23:24 -0300

    aula-28

--- arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -2,7 +2,6 @@ package io.github.cursodsousa.arquiteturaspring.montadora.api;
 
 import io.github.cursodsousa.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
-    @Qualifier("motorTurbo")
+    @Turbo
     private Motor motor;
 
     @PostMapping
