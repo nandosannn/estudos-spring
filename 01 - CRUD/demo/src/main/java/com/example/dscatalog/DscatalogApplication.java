f35526205b39cdb25ec772e3cc548ec4c9b464e4
nandosannn <silvafernandosan@gmail.com>
2025-04-20 19:26:57 -0300

    classe category

--- 01 - CRUD/demo/src/main/java/com/example/dscatalog/DscatalogApplication.java
+++ 01 - CRUD/demo/src/main/java/com/example/dscatalog/DscatalogApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DscatalogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
 }
