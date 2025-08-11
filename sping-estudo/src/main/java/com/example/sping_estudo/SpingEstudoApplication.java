4b6783ef67cf6bb583bbb677c23cab61c8b46c1f
Fernando Santos <158214975+nandosannn@users.noreply.github.com>
2025-08-11 20:42:19 -0300

    feat: retomando os estudos

--- sping-estudo/src/main/java/com/example/sping_estudo/SpingEstudoApplication.java
+++ sping-estudo/src/main/java/com/example/sping_estudo/SpingEstudoApplication.java
@@ -1,13 +1,13 @@
-package com.example.dscatalog;
+package com.example.sping_estudo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscatalogApplication {
+public class SpingEstudoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DscatalogApplication.class, args);
+		SpringApplication.run(SpingEstudoApplication.class, args);
 	}
 
 }
